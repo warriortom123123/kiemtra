ca6be0f1110c0f68182cb08f085506fa1f66607d
Duy Tân Đỗ <taigaaisaka192002@gmail.com>
2023-03-23 10:57:37 +0700

    first commit

---
+++ Application/Interfaces/IApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Master;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDBContext
+    {
+        DbSet<AppSetting> AppSettings { get; set; }
+        Task<int> SaveChangesAsync();
+    }
+}
